ff9ba4afb703a5d960f21d723648d4e943aba63e
kiprono <im.kiprono@gmail.com>
2024-03-20 15:07:45 +0300

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar"
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
+
 
 
 
@@ -13,23 +15,16 @@ const App = () => {
     <Navbar/>
 
     <Hero />
+    
 
     <HomeCards /> 
+
     <JobListings/>
+
+
+    <ViewAllJobs /> 
   
 
-    
-   
-  
-    
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
 
 
 
