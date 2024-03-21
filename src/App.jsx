d185522e64f298a7f3957dbea6b5d6de8f57ff26
kiprono <im.kiprono@gmail.com>
2024-03-21 20:12:52 +0300

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
 
 
@@ -30,6 +31,19 @@ const App = () => {
      }); 
      return;
   }
+  //update job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+  
+      },
+      body: JSON.stringify(job),
+     }); 
+     return;
+
+  }
    
   
   const router = createBrowserRouter(
@@ -40,6 +54,7 @@ const App = () => {
       <Route path='/jobs' element={<JobsPage />} />
      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
       <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader} />
+      <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob}/>} loader={jobLoader} />
       <Route path='*' element={<NotFoundPage />} />
   
       </Route>
