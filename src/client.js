7aa1b75264b6cb5d3e9149e7dad104a7abcdfc73
Emmanuel Kiprono <42340517+ekck@users.noreply.github.com>
2024-04-02 16:53:32 +0300

    Update client.js

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import {createClient} from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-    projectId: 'yi89pfyg',
+    projectId: '',
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
-    token: 'skzzMXHUxuXTmuM71EetFoxKNsvdlVZBf293yx34vQOlAeNph0sZ5Ld6GSdB2ATkaCj2JFTxJU5WJG1cswQW0cW0O53qS5x9dQ5iRpJnEjv9GmT97ia2gKMBphaGtiCMhJ52fyzkTaP4WE9g953i9Fjo0nW2hDppac0NWTiTd5xvV8gLmY7k',
+    token: '',
 })
 
 const builder = imageUrlBuilder(client);
